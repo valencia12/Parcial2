fa7c8d8e8bb2ef03bdfa9a4372ef1d452d0c923a
Jorge <Jorge@Jorge-pc>
2018-06-06 20:58:21 -0600

    Voodolands

--- src/Voodolands/FactoryVoodolands.java
+++ src/Voodolands/FactoryVoodolands.java
@@ -5,10 +5,32 @@
  */
 package Voodolands;
 
+import AbstracFactory.AbstractFactory;
+
 /**
  *
  * @author Jorge
  */
-public class FactoryVoodolands {
+public class FactoryVoodolands implements AbstractFactory {
+
+    @Override
+    public String getDemacia(int a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNoxus(int b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getVoodolands(int c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getVehiculos(int d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
