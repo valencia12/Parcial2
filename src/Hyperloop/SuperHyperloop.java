e4cbebc9f3aaf93d3d38986bd04ea5daa27e9945
Jorge <Jorge@Jorge-pc>
2018-06-08 13:42:38 -0600

    SuperHyperloop

---
+++ src/Hyperloop/SuperHyperloop.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hyperloop;
+
+/**
+ *
+ * @author Jorge
+ */
+public class SuperHyperloop {
+    private int vida;
+    private int ataque;
+    private int tiempo;
+
+    public SuperHyperloop() {
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    
+    
+    
+}
