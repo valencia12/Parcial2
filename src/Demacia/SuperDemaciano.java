b87f485af822b64b6cb008f3896fa8b986c40b06
Jorge <Jorge@Jorge-pc>
2018-06-08 13:31:08 -0600

    SuperDemaciano

---
+++ src/Demacia/SuperDemaciano.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Demacia;
+
+/**
+ *
+ * @author Jorge
+ */
+public class SuperDemaciano {
+    private int vida;
+    private int ataque;
+    private int tiempo;
+    
+    private SuperDemaciano(){}
+
+    public SuperDemaciano(int vida, int ataque, int tiempo) {
+        this.vida = vida;
+        this.ataque = ataque;
+        this.tiempo = tiempo;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    
+    
+    
+    
+    
+}
